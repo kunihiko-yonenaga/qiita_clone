ba12db76d70bbbf95fbdba08f458c9bb3bdcd449
Kunihiko Yonenaga <kuni@YKnoMBP.local>
2019-10-19 20:25:06 +0900

    validation追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :body
+  end
+
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
