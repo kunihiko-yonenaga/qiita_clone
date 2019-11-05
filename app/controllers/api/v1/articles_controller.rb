712f0f3a398f38a8c3761443d52583620a05858c
kunihiko-yonenaga <rabessi.coo@gmail.com>
2019-11-05 14:32:49 +0900

    記事作成のcontrollerの修正

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -11,6 +11,7 @@ class Api::V1::ArticlesController < Api::V1::BaseApiController
   end
 
   def create
+    binding.pry
     @article = current_user.articles.create!(article_params)
     render json: @article
   end
--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::BaseApiController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  def loggedinuser
-    @current_user = user.first
-  end
+
 end
