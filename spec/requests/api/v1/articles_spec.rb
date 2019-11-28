cdc2c7d0efb037d719a263fcced0f24ef99aac32
kunihiko-yonenaga <rabessi.coo@gmail.com>
2019-11-28 21:47:13 +0900

    記事更新と記事削除のAPI実装とそのテストコード

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -69,4 +69,33 @@ RSpec.describe "Articles", type: :request do
       expect(res["body"]).to eq params[:article][:body]
     end
   end
+
+  describe "PATCH /articles/:id" do
+    subject { patch(api_v1_article_path(article_id), params: params) }
+
+    let(:params) { { article: attributes_for(:article) } }
+    let(:article) { create(:article) }
+    let(:article_id) { article.id }
+
+    it "articleのレコード更新ができる" do
+      expect { subject }.to change { Article.find(article.id).title }.from(article.title).to(params[:article][:title]) &
+                              change { Article.find(article.id).body }.from(article.body).to(params[:article][:body])
+        expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "DELETE /api/v1/articles/:id" do
+    subject { delete(api_v1_article_path(article.id)) }
+
+    let!(:article) { create(:article, user_id: current_user.id) }
+    let(:current_user) { create(:user) }
+    before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user) }
+
+    it "articleのレコードを削除できる" do
+       expect { subject }.to change { current_user.articles.count }.by(-1)
+       expect(response).to have_http_status(:success)
+    end
+  end
+
+
 end
